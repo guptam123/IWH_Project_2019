275bb880f5167c4393fa9c68e09956b9ff835eae
guptakj <35427463+guptakj@users.noreply.github.com>
2019-05-07 13:18:13 +0530

    create post and show post api

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const routes = require('./routes/routes');
 
 var app = express();
 app.get('/', (req, res) => {
     res.send('Hello Express')
 });
+
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://arcade:4mlabACC@firstcluster-yc1pt.mongodb.net/virago?retryWrites=true';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+/*app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));*/
+app.use('/api', routes);
+
 //app.listen(process.env.PORT || 3000)
 
 let port = process.env.PORT || 1235;
---
+++ controllers/controller.js
@@ -0,0 +1,47 @@
+const Post = require('../models/post');
+const Log = require('../models/log');
+
+
+exports.post_create = function (req, res) {
+    let post1 = new Post(
+        {
+            title: req.body.title,
+            content: req.body.content,
+            username: req.body.username,
+            companyid: req.body.companyid,
+            upvote: req.body.upvote,
+
+        }
+    );
+    var tagString=req.body.tag;
+    var tags= tagString.split(",");
+    for(var i=0;i<tags.length;i++)
+    {
+        //console.log(tags[i]+"\n");
+        post1.tag.push(tags[i]);
+    }
+    post1.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('post Created successfully')
+    });
+    var logItem=req.body.username + " created a new post";
+    let log = new Log(
+        {
+            item:logItem
+        });
+    log.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+    })
+};
+exports.show_posts = function (req , res) {
+    Post.find({}).then(function (posts) {
+        res.send(posts);
+    });
+};
+exports.show_posts = function (req , res) {
+    res.send('postman is working')
+};
