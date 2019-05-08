5f21ade55f7b283ac0c19f7c1c530b620a710343
guptakj <35427463+guptakj@users.noreply.github.com>
2019-05-08 10:58:42 +0530

    change post api to get api of all search apis

--- routes/routes.js
+++ routes/routes.js
@@ -15,18 +15,23 @@ router.post('/post_create/',controller.post_create);
 
 //router.post('/:id/add_comment', controller.add_comment);
 
-router.post('/search/people',controller.search_people);
+router.post('/user_profile_updation/', userProfileController.user_profile_updation);
 
-router.post('/search/company',controller.search_company);
 
-router.post('/search/post',controller.search_post);
 
-router.post('/search/global_search',controller.global_search);
 //get API
 
 router.get('/show_posts', controller.show_posts);
+
 //router.get('/:uid/:aid/deletepost_user', controller.deletepost_user);
 
+router.get('/search/people',controller.search_people);
 
-router.post('/user_profile_updation/', userProfileController.user_profile_updation);
+router.get('/search/company',controller.search_company);
+
+router.get('/search/post',controller.search_post);
+
+router.get('/search/global_search',controller.global_search);
+
+
 module.exports = router;
